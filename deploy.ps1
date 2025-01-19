25eaa7fa51c982d2a223e8c73bfe0d93d6ef0fb0
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2025-01-19 01:51:28 -0700

    removed powershell script

--- deploy.ps1
+++
@@ -1,3 +0,0 @@
-docker build -t hangman-game .
-docker tag hangman-game registry.digitalocean.com/blazium-registry/hangman-game:latest
-docker push registry.digitalocean.com/blazium-registry/hangman-game:latest
